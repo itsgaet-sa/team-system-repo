be93cb3143516733d05c892f8381439f0972397e
itsgaet-sa <bear27orso@gmail.com>
2025-10-06 05:49:39 +0000

    Refactor update_metadata.py to retrieve hostname and IPv4 directly instead of using environment variables

--- morpheus_scripts/update_metadata.py
+++ morpheus_scripts/update_metadata.py
@@ -1,13 +1,31 @@
 import os
 import json
+import socket
 
-# Morpheus di solito passa le variabili come variabili d'ambiente
-hostname = os.getenv('hostname')
-ipv4 = os.getenv('ipv4')
-url = os.getenv('url')
+# Recupera hostname
+hostname = socket.gethostname()
+
+# Recupera ipv4 (prima interfaccia non localhost)
+def get_ipv4():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # Connessione fittizia per ottenere l'IP locale
+        s.connect(('8.8.8.8', 80))
+        ip = s.getsockname()[0]
+    except Exception:
+        ip = '127.0.0.1'
+    finally:
+        s.close()
+    return ip
+
+ipv4 = get_ipv4()
+
+
+# Costruisci l'URL come richiesto: hostname + '.easyfattincloud.it'
+url = f"http://{hostname}.easyfattincloud.it"
+
 dominio = 'easyfattincloud'
 
-# Costruisci il dizionario dei metadata
 metadata = {
     'hostname': hostname,
     'ipv4': ipv4,
@@ -15,7 +33,6 @@ metadata = {
     'dominio': dominio
 }
 
-# Stampa i metadata in formato JSON (o salva su file, se richiesto da Morpheus)
 print(json.dumps(metadata, indent=2))
 
 # Se serve scrivere su file, decommenta la riga seguente:
