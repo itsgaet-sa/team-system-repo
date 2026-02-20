7cb1df9b2d3fde53c698edfaa2003f18b8745cb1
f-caiati <160047832+f-caiati@users.noreply.github.com>
2026-02-20 23:45:57 +0100

    Add port specification to New-PSSession command

--- morpheus_scripts/deso_efc_user_migration.ps1
+++ morpheus_scripts/deso_efc_user_migration.ps1
@@ -152,11 +152,13 @@ if ($TEST_MODE) {
     Write-Output "[INFO] Connessione al server dispatcher ($migrationServerIP) in corso..."
     $session = $null
     try {
-        $session = New-PSSession -HostName $migrationServerIP `
-                         -UserName $migrationUserRaw `
-                         -Credential $migrationCred `
-                         -SSHTransport `
-                         -ErrorAction Stop
+        $session = New-PSSession `
+            -HostName $migrationServerIP `
+            -UserName $migrationUserRaw `
+            -SSHTransport `
+            -Credential $migrationCred `
+            -Port 22 `
+            -ErrorAction Stop
         Write-Output "[SUCCESS] Sessione remota stabilita con $migrationServerIP"
     } catch {
         Write-Output "[ERROR] Impossibile connettersi al server dispatcher ($migrationServerIP)"
