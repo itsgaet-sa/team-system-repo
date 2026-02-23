bb8cb857c47b11d7b78cd527f97f0b668162c7bc
Gaetano Maurizio Abbaticchio <91220051+ItsGaet@users.noreply.github.com>
2026-02-23 18:01:22 +0100

    Update deso_efc_user_migration.ps1

--- morpheus_scripts/deso_efc_user_migration.ps1
+++ morpheus_scripts/deso_efc_user_migration.ps1
@@ -78,8 +78,8 @@ $migrationKeyBase64 = '<%=cypher.read("secret/EFC-TS_MIG_DANEA_SSH",true)%>'
 # PATH REMOTI SUL DISPATCHER
 # ──────────────────────────────────────────────────────────────────────────────
 $migrationServerIP = "10.182.1.11"
-$remoteQueuePath   = "D:\tools\migration-tool\incoming"
-$remoteDispatcher  = "D:\tools\migration-tool\dispatcher.ps1"
+$remoteQueuePath   = "D:\tools\migration-tool-st\incoming"
+$remoteDispatcher  = "D:\tools\migration-tool-st\dispatcher.ps1"
 
 # ──────────────────────────────────────────────────────────────────────────────
 # INIZIO SCRIPT
